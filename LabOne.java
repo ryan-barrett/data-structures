1972e354c0a68ef67262fce606d74a700c3e071a
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-08-24 06:46:05 -0400

    lab one

---
+++ LabOne.java
@@ -0,0 +1,35 @@
+public class LabOne {
+
+    public static void main(String[] args) {
+        calcSeries(5);
+
+        int newArr[] = { 1, 2, 3, 4, 5 };
+        largestSmallest(newArr);
+
+    }
+
+    public static void calcSeries(int series) {
+        int sum = 0;
+
+        while (series > 0) {
+            sum += Math.pow(series, 2);
+            series--;
+        }
+        System.out.println("series sum: " + sum);
+    }
+
+    public static void largestSmallest(int[] arr) {
+        int largest = 0;
+        int smallest = arr[0];
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > largest) {
+                largest = arr[i];
+            } else if (arr[i] < smallest) {
+                smallest = arr[i];
+            }
+        }
+        System.out.println("largest: " + largest);
+        System.out.println("smallest: " + smallest);
+    }
+}
