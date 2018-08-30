27f3168ba7feef70fe868c6c5ee2cb603a476132
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-08-30 13:35:36 -0400

    lab two spacing fix

--- LabTwo.java
+++ LabTwo.java
@@ -2,16 +2,14 @@ import java.util.ArrayList;
 
 public class LabTwo {
     
-    public static void main(String[] args){
+    public static void main(String[] args) {
         
         ArrayList<Integer> newArr = new ArrayList<Integer>();
         modifyArray(newArr, 4);
         System.out.println(newArr);
         
         int newArrTwo[] = {1,2,3,4,5,6,7,8,9,10};
-        linearSearch(newArrTwo, 5);
-        
-        
+        linearSearch(newArrTwo, 5); 
     }  
     
     
@@ -19,11 +17,10 @@ public class LabTwo {
        arr.add(toBeAdded);
        System.out.println(toBeAdded + " added to array"+ arr);
        arr.remove(0);
-       System.out.println("First element removed from array. Current array: " + arr);
-       
+       System.out.println("First element removed from array. Current array: " + arr);   
     }
     
-    public static void linearSearch(int[] arr, int targetValue){
+    public static void linearSearch(int[] arr, int targetValue) {
         int foundIndex = -1;
         
         for (int i = 0; arr.length > i; i++) {
