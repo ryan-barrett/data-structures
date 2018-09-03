01aa2d4544028312a3d844f2635ddfcf034e3afa
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-09-03 15:49:54 -0400

    completed sum of series

---
+++ Assignments/Assignment_One/SumOfSeries.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class SumOfSeries {
+
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+
+        System.out.println("Enter number to sum: ");
+        int sumThis = myScan.nextInt();
+        addSum(sumThis);
+        myScan.close();
+    }
+
+    public static void addSum(int series) {
+        int sum = 0;
+
+        while (series > 0) {
+            sum += Math.pow(series, 2);
+            series--;
+        }
+        System.out.println("series sum: " + sum);
+    }
+}
