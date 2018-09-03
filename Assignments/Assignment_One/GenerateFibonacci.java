f9d2b99b0b52a7c965563c0eda8a7667016b2434
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-09-03 17:45:52 -0400

    solved create fibonacci series between two intervals

---
+++ Assignments/Assignment_One/GenerateFibonacci.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class GenerateFibonacci {
+
+    public static void main(String[] args) {
+        Scanner myScan = new Scanner(System.in);
+
+        System.out.println("Enter starting number: ");
+        int startSeries = myScan.nextInt();
+        System.out.println("Enter ending number: ");
+        int endSeries = myScan.nextInt();
+
+        createFibSeries(startSeries, endSeries);
+        myScan.close();
+    }
+
+    public static void createFibSeries(int start, int end) {
+        ArrayList<Integer> fibArr = new ArrayList<Integer>();
+        int first = 0;
+        int second = 1;
+        int third = 1;
+
+        while (first <= end) {
+            if (first >= start) {
+                fibArr.add(first);
+            }
+
+            first = second;
+            second = third;
+            third = first + second;
+        }
+
+        System.out.println("Generated Fibonacci series: " + fibArr);
+    }
+}
